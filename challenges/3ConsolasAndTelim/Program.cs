ed693123c730b9c707ce462b318888eb1921a2a1
cjw <choij2015@gmail.com>
2023-06-03 10:18:26 +0900

    Consolas and Telim challenge

---
+++ challenges/3ConsolasAndTelim/Program.cs
@@ -0,0 +1,9 @@
+﻿// Bread is ready.
+// Who is the bread for?
+// RB
+// Noted: RB got bread.
+
+Console.WriteLine("Bread is ready.");
+Console.WriteLine("Who is the bread for?");
+string person = Console.ReadLine();
+Console.WriteLine("Noted: " + person + " got bread.");
