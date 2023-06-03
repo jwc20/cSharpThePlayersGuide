74d19e34f9100ddb11c0052b81ae845daaff3b92
cjw <choij2015@gmail.com>
2023-06-03 12:33:09 +0900

    The Thing Namer 300 challenge

---
+++ challenges/4TheThingNamer3000/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine(); // The thing are talking about.
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+
+string b = Console.ReadLine(); // The thing's description.
+string c = "of Doom";
+string d = "3000";
+
+Console.WriteLine("The " + b + " " + a + " " + c + " " + d + "!"); // Combines the strings.
+
+
+// Better variable names would make it understandable.
