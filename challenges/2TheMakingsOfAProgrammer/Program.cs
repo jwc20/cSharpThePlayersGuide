45735710aebe74122c5edd07729c9d0cfc92f5f9
cjw <choij2015@gmail.com>
2023-06-03 09:51:13 +0900

    The Makings of a Programmer challenge

---
+++ challenges/2TheMakingsOfAProgrammer/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("I");
+Console.WriteLine("am");
+Console.WriteLine("the");
+Console.WriteLine("FUCKING");
+Console.WriteLine("chosen");
+Console.WriteLine("one!");
+
+// A program can contain an infinite amount of statements.
