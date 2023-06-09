5e045ebd12a2b6c05074a4476d88f9c57a95c2f0
cjw <choij2015@gmail.com>
2023-06-09 21:11:56 +0900

    The Dominion of Kings challenge

---
+++ challenges/9TheDominionOfKings/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Provinces: ");
+int provinces = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Duchies: ");
+int duchies = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Estates: ");
+int estates = Convert.ToInt32(Console.ReadLine());
+
+int score = (estates) + (3 * duchies) + (6 * provinces);
+
+
+Console.WriteLine(score);
