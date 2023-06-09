382c4325b902753f7aec0db5ae5668d3eccd709d
cjw <choij2015@gmail.com>
2023-06-09 20:37:55 +0900

    The Triangle Farmer challenge

---
+++ challenges/7TheTriangleFarmer/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string baseOfTriangleString;
+string heightOfTriangleString;
+
+
+Console.WriteLine("Enter the base of triangle: ");
+baseOfTriangleString = Console.ReadLine();
+int baseOfTriangle = Convert.ToInt32(baseOfTriangleString);
+
+Console.WriteLine("Enter the base of triangle: ");
+heightOfTriangleString = Console.ReadLine();
+int heightOfTriangle = Convert.ToInt32(heightOfTriangleString);
+
+float area = (baseOfTriangle * heightOfTriangle) / 2;
+
+Console.WriteLine(area);
