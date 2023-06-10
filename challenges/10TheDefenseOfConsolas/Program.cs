82104419d2f97eb3c4f51de601d0bc06afa8cd67
cjw <choij2015@gmail.com>
2023-06-10 11:25:09 +0900

    .

--- challenges/10TheDefenseOfConsolas/Program.cs
+++ challenges/10TheDefenseOfConsolas/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Data.Common;
-
-Console.Title = "Defense of Consolas";
+﻿Console.Title = "Defense of Consolas";
 
 Console.Write("Target Row? ");
 int targetRow = Convert.ToInt32(Console.ReadLine());
@@ -9,8 +7,6 @@ Console.Write("Target Column? ");
 int targetColumn = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Deploy to: ");
-
-
 Console.BackgroundColor = ConsoleColor.Blue;
 Console.WriteLine("(" + targetRow + ", " + (targetColumn - 1) + ")");
 Console.BackgroundColor = ConsoleColor.Yellow;
@@ -26,4 +22,3 @@ Console.WriteLine("(" + (targetRow + 1) + ", " + targetColumn + ")");
 //Console.WriteLine($"({row + 1}, {column})");
 
 Console.Beep();
-
