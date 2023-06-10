9c91103f648fbfa81a0be331238379098757dc4e
cjw <choij2015@gmail.com>
2023-06-10 10:28:31 +0900

    Level 8 scratch work

---
+++ challenges/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿int health = -10;
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine(Math.Clamp(health, -5, 100));
+// Console.Beep(440, 1000);
+
+Console.WriteLine($"{Math.PI:0.000}");
+
+
+Console.WriteLine("Press any key when you're ready to begin.");
+Console.ReadKey(true);
