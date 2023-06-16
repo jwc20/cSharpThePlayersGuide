943763dc019248ada0dabff4bed16f23a9f6421a
jwc20 <choij2015@gmail.com>
2023-06-16 21:24:10 +0900

    Repairing the Clock Tower challenge

---
+++ challenges/11RepairingTheClockTower/RepairingTheClockTower/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RepairingTheClockTower
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+      {
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num % 2 == 1)
+          Console.WriteLine("Tick");
+        else
+          Console.WriteLine("Tock");
+      }
+  }
+}
